5efb9ada94368708e9707e64acb9115618992e24
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-28 12:48:01 +0900

    add util

---
+++ util.go
@@ -0,0 +1,31 @@
+package GFileable
+
+import "os"
+
+// Write string to file
+func (f Fileable) Write(string string) error {
+	file, err := os.OpenFile(f.path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	data := []byte(string)
+	_, err = file.Write(data)
+
+	defer file.Close()
+
+	return nil
+}
+
+// Check files existence
+func (f Fileable) Check(files ...string) bool {
+
+	for f := range files {
+		_, err := os.Stat(files[f])
+		if err != nil {
+			return false
+		}
+	}
+
+	return true
+}
