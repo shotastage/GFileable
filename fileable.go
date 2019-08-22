5ceafb8914a8a5381c5c97495e67b8a1224afc37
Shota Shimazu <hornet.live.mf@gmail.com>
2019-08-22 17:31:47 +0900

    add home

--- fileable.go
+++ fileable.go
@@ -1,6 +1,10 @@
 package GFileable
 
-import "os"
+import (
+	"os"
+	"os/user"
+	"strings"
+)
 
 // Fileable is a interface for creating Fileable object.
 type Fileable struct {
@@ -9,6 +13,24 @@ type Fileable struct {
 
 // Path is a constructor.
 func Path(path string) *Fileable {
+
+	if strings.Contains(path, "~") || strings.Contains(path, "$HOME") {
+
+		user, err := user.Current()
+
+		if err != nil {
+			panic("Failed to get home dir")
+		}
+
+		home := user.HomeDir
+
+		hpath := strings.Replace(path, "~", home, 1)
+
+		hpath = strings.Replace(path, "$HOME", home, 1)
+
+		return &Fileable{Path: hpath}
+
+	}
 
 	if path == "" {
 		current, _ := os.Getwd()
